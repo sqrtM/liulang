e84482fb1d6f6be0d98b0b6932ec2d8209c0b5c8
Mason Pike <m@sonpike.net>
2025-06-10 21:30:04 +0200

    redo it all

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,3 @@
-use utils::Pipeline;
-
-use crate::parser::TokenData;
-
-use std::env;
-
-mod analyzer;
-mod interpreter;
-mod parser;
-mod repl;
-mod utils;
-
 fn main() {
-    utils::show_license_notice();
-    match env::args().collect::<Vec<String>>().get(1) {
-        Some(path) => {
-            let pipeline = Pipeline::new(path.into());
-            pipeline.run()
-        }
-        None => loop {
-            let expressions = repl::evaluate();
-            for expression in expressions {
-                println!("{:?}", interpreter::flatten(&expression));
-            }
-        },
-    };
+    println!("Hello, world!");
 }
