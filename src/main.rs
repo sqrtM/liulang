deb4503b7a211bbbe15d44726037d3b1b631be3d
Mason Pike <m@sonpike.net>
2025-06-13 21:51:55 +0200

    redo the expressionizing / parsing

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,26 @@
-mod analyzer;
+use std::{
+    fs::File,
+    io::{self, BufRead},
+};
+
+use crate::{parser::expressionize, tokenizer::TokenData};
+
 mod parser;
+mod tokenizer;
 fn main() {
-    println!("Hello, world!");
+    let file = File::open("test.liu").unwrap();
+    let lines: Vec<String> = io::BufReader::new(file)
+        .lines()
+        .map_while(Result::ok)
+        .collect();
+
+    let token_data: Vec<TokenData> = lines
+        .iter()
+        .enumerate()
+        .flat_map(|(i, line)| tokenizer::tokenize(line, i))
+        .collect();
+
+    let expressions = expressionize(&token_data, 0);
+
+    println!("{:?}", expressions);
 }
